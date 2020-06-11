2a1a42221b8ba23c7ac8a7f6237435b6349b2b34
Kundan <kundank191@gmail.com>
2020-06-11 15:57:25 +0530

    update the frontend

--- main.py
+++ main.py
@@ -7,83 +7,57 @@ Created on Sun Jun  7 11:09:33 2020
 
 @author: k
 """
-from flask import Flask, request
+from flask import Flask, request, render_template
 import pandas as pd
 import pickle
-from flasgger import Swagger
 
 # From here the app will start
-app = Flask(__name__)
-Swagger(app)
-
+app = Flask(__name__, template_folder = 'templates')
 # Load the pickle file, get the classifier
 pickle_in = open('model/classifier.pkl','rb')
 classifier = pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
-    return 'Find out if a note is fake or not.'
+    return render_template('main.html')
 
-@app.route('/predict')
-def predict_note_genuity():
-    """Check genuity of a single Bank note
-    Ui made using DocString
-    ---
-    parameters:
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values   
-    """
-    variance = request.args.get('variance')
-    skewness = request.args.get('skewness')
-    curtosis = request.args.get('curtosis')
-    entropy  = request.args.get('entropy')
+@app.route('/predict', methods = ['POST'])
+def predict():
+    variance = request.form['variance']
+    skewness = request.form['skewness']
+    curtosis = request.form['curtosis']
+    entropy  = request.form['entropy']
     # using these variables to predict if the note was genuine
     prediction = classifier.predict([[variance,
                                       skewness,
                                       curtosis,
                                       entropy]])
-    
-    if prediction == 1:
-        return "Genuine note"
-    else:
-        return "Fake note"
+    result = get_formatted_prediction(prediction)
+    return render_template('result.html',result = result)
 
-@app.route('/predict_file', methods = ['POST'])
-def predict_file_genuity():
-    """Check genuity of a file containing list of notes
-    Just using DocStrings
-    ---
-    parameters:
-      - name: file
-        in: formData
-        type: file
-        required: true
-    responses:
-        200:
-            description : The output values
-        
-    """
-    df_test = pd.read_csv(request.files.get('file'))
-    predictions = classifier.predict(df_test)
-    return "Predicted Values for the file " + str(list(predictions))
+@app.route('/predict_file', methods = ['GET','POST'])
+def predict_file():
+    # read the file
+    df_test = pd.read_csv(request.files['file'])
+    # do the prediction
+    prediction = classifier.predict(df_test)
+    # format the result
+    result = get_formatted_prediction(prediction)
+    return render_template('result.html', result = result)
     
+def get_formatted_prediction(prediction):
+    if len(prediction) == 1:
+        if prediction[0] == 0:
+            return "It is a Fake note"
+        else:
+            return "It is a Genuine note"
+    result = ""
+    for i in range(len(prediction)):
+        if prediction[i] == 0:
+            result += str(i+1) + " is a Fake note" +"\n"
+        else:
+            result += str(i+1) + " is a Genuine note" +"\n"
+    return result
 if __name__ == '__main__':
     app.run()
 
